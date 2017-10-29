0efe455e652d0e622cbac50740243e14e5795f99
Tyran <15055160265@163.com>
2017-10-29 23:11:15 +0800

    init

---
+++ src/components/LayoutHeader/DocSearch.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * @emails react-core
+ */
+
+import React, {Component} from 'react';
+import {colors, media,fonts} from 'theme';
+
+class DocSearch extends Component {
+  state = {
+    enabled: true,
+  };
+  render() {
+    const {enabled} = this.state;
+
+    return enabled ? (
+      <div
+        css={{
+          display: 'flex',
+          flex: '0 0 auto',
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingLeft: '0.5rem',
+          paddingRight: '0.5rem',
+          fontSize:15,
+          cursor: 'pointer',
+          [media.lessThan('small')]: {
+            justifyContent: 'flex-end',
+          },
+          [media.lessThan('large')]: {
+            marginRight: 10,
+          },
+          [media.between('small', 'medium')]: {
+            width: 'calc(100% / 3)',
+          },
+          [media.between('medium', 'xlarge')]: {
+            width: 'calc(100% / 6)',
+          },
+          [media.greaterThan('small')]: {
+            minWidth: 20,
+          },
+        }}>
+        <a
+          css={{
+            padding: '5px 10px',
+            marginLeft: 10,
+            whiteSpace: 'nowrap',
+            ...fonts.small,
+
+            ':hover': {
+              color: colors.brand,
+            },
+
+            ':focus': {
+              outline: 0,
+              backgroundColor: colors.lighter,
+              borderRadius: 15,
+            },
+          }}
+          href='https://github.com/lit-forest'
+          target="_blank"
+          rel="noopener">
+          <i className="icon-github"></i>
+        </a>
+      </div>
+    ) : null;
+  }
+}
+
+export default DocSearch;
---
+++ src/components/LayoutHeader/Header.js
@@ -0,0 +1,200 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * @emails react-core
+ */
+
+'use strict';
+
+import Container from 'components/Container';
+import HeaderLink from './HeaderLink';
+import Link from 'gatsby-link';
+import React from 'react';
+import {colors, fonts, media} from 'theme';
+import {version} from 'site-constants';
+import ExternalLinkSvg from 'templates/components/ExternalLinkSvg';
+import DocSearch from './DocSearch';
+
+import logoSvg from 'icons/logo.jpg';
+
+const Header = ({location}) => (
+  <header
+    css={{
+      backgroundColor: colors.darker,
+      color: colors.white,
+      position: 'fixed',
+      zIndex: 1,
+      width: '100%',
+      top: 0,
+      left: 0,
+    }}>
+    <Container>
+      <div
+        css={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          height: 50,
+          [media.between('small', 'large')]: {
+            height: 45,
+          },
+          [media.lessThan('small')]: {
+            height: 40,
+          },
+        }}>
+        <Link
+          css={{
+            display: 'flex',
+            marginRight: 10,
+            height: '100%',
+            alignItems: 'center',
+            color: colors.brand,
+
+            ':focus': {
+              outline: 0,
+              color: colors.white,
+            },
+
+            [media.greaterThan('small')]: {
+              width: 'calc(100% / 3)',
+            },
+            [media.lessThan('small')]: {
+              flex: '0 0 auto',
+            },
+          }}
+          to="/">
+          <img src={logoSvg} alt="" height="25" />
+          <span
+            css={{
+              color: '#fff',
+              fontFamily: '"Source Sans Pro","Helvetica Neue",Helvetica,Arial,sans-serif',
+              marginLeft: 10,
+              fontWeight: 500,
+              fontSize: 14,
+              lineHeight: '20px',
+              [media.lessThan('large')]: {
+                fontSize: 16,
+                marginTop: 1,
+              },
+              [media.lessThan('small')]: {
+                // Visually hidden
+                position: 'absolute',
+                overflow: 'hidden',
+                clip: 'rect(0 0 0 0)',
+                height: 1,
+                width: 1,
+                margin: -1,
+                padding: 0,
+                border: 0,
+              },
+            }}>
+            little forest
+          </span>
+        </Link>
+
+        <nav
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'stretch',
+            overflowX: 'auto',
+            overflowY: 'hidden',
+            WebkitOverflowScrolling: 'touch',
+            height: '100%',
+            width: '60%',
+
+            [media.size('xsmall')]: {
+              flexGrow: '1',
+              width: 'auto',
+            },
+            [media.greaterThan('xlarge')]: {
+              width: null,
+            },
+            [media.lessThan('small')]: {
+              maskImage:
+                'linear-gradient(to right, transparent, black 20px, black 90%, transparent)',
+            },
+          }}>
+          <HeaderLink
+            isActive={location.pathname.includes('/docs/')}
+            title="HOME"
+            to="/docs/hello-world.html"
+          />
+          <HeaderLink
+            isActive={location.pathname.includes('/tutorial/')}
+            title="ARCHIVE"
+            to="/tutorial/tutorial.html"
+          />
+          <HeaderLink
+            isActive={location.pathname.includes('/community/')}
+            title="CATEGORIES"
+            to="/community/support.html"
+          />
+          <HeaderLink
+            isActive={location.pathname.includes('/blog')}
+            title="RESUME"
+            to="/blog/"
+          />
+        </nav>
+        <DocSearch/>
+        <div
+          css={{
+            [media.lessThan('medium')]: {
+              display: 'none',
+            },
+            [media.greaterThan('large')]: {
+              width: 'calc(100% / 6)',
+            },
+          }}>
+
+          <a
+            css={{
+              padding: '5px 10px',
+              marginLeft: 10,
+              whiteSpace: 'nowrap',
+              ...fonts.mid,
+
+              ':hover': {
+                color: colors.brand,
+              },
+
+              ':focus': {
+                outline: 0,
+                backgroundColor: colors.lighter,
+                borderRadius: 15,
+              },
+            }}
+            href="https://twitter.com/MelloZhao"
+            target="_blank"
+            rel="noopener">
+            <i className="icon-twitter"></i>
+          </a>
+          <a
+            css={{
+              padding: '5px 10px',
+              marginLeft: 10,
+              whiteSpace: 'nowrap',
+              ...fonts.small,
+
+              ':hover': {
+                color: colors.brand,
+              },
+
+              ':focus': {
+                outline: 0,
+                backgroundColor: colors.lighter,
+                borderRadius: 15,
+              },
+            }}
+            href="mailto:wrsden@gmail.com?subject=little forest"
+            target="_blank"
+            rel="noopener">
+            <i className="icon-brand"></i>
+          </a>
+        </div>
+      </div>
+    </Container>
+  </header>
+);
+
+export default Header;
