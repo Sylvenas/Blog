18f5da86a30746acfbe24c730e80a24384bd3e36
Tyran <15055160265@163.com>
2017-11-30 11:35:43 +0800

    closure

--- test/test.js
+++ test/test.js
@@ -1,10 +1,5 @@
-const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
-
-const fn1 = s => s.toLowerCase();
-const fn2 = s => s.split('').reverse().join('');
-const fn3 = s => s + '!'
-
-const newFunc = pipe(fn1, fn2, fn3);
-const result = newFunc('Time'); // emit!
-
-console.log(toSlug('JS Cheerleader')); // 'js-cheerleader'
+const secret = function (msg) {
+    return function () {
+        return msg
+    }
+}
