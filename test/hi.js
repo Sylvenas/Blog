b0bc28216de04c4a630cf810e69151aa08bee62c
sylvenas <sylvenas@outlook.com>
2018-06-09 10:23:41 +0800

    闭包

--- test/hi.js
+++ test/hi.js
@@ -1,6 +1,36 @@
-document.querySelector('button').onclick = function open() {
-    const newPage = window.open('', '_blank')
-    axios.get('https://unpkg.com/axios/dist/axios.min.js').then(res => {
-        newPage.location.href = 'https://www.baidu.com'
-    })
+var pingpong = (function () {
+    var private = 0;
+
+    return {
+        inc(n) {
+            return private += n
+        },
+        dec(n) {
+            return private -= n
+        }
+    }
+})();
+
+pingpong.div = function (n) {
+    return private / n
 }
+
+var log = console.log;
+
+log(pingpong.inc(10));
+
+log(pingpong.dec(3));
+
+
+function showObject(obj) { 
+    return function () { 
+        return obj
+    }
+}
+
+var o = { a: 1 };
+var showO = showObject(o);
+showO()  // {a:1}
+
+o.a = 6;
+showO()  // {a:6}
