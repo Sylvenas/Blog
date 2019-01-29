0ca51377a0c40e5a063557597a6e028c126ef39a
hzzhaoxiangtao <hzzhaoxiangtao@corp.netease.com>
2019-01-29 10:08:54 +0800

    resume页面的love图片的高度塌陷导致的闪烁问题，添加媒体查询

--- README.md
+++ README.md
@@ -1,66 +1,51 @@
-# reactjs.org
+# Blog
+## TODO List
 
-This repo contains the source code and documentation powering [reactjs.org](https://reactjs.org/).
+### Books
+
+#### 计算机原理
+- 《编码——隐匿在计算机软硬件背后的语言》
+- 《编译原理》
+- 《数据结构》
+
+#### HTTP & HTTP2 & HTTPS
+- 浏览器缓存/代理服务器缓存/CDN
+- HTTP2的特性
+- HTTPS安全的原因，三个要素的原理
+- 常见的HTTP攻击与防御
+
+#### 性能优化
+- 浏览器的优化
+  - 浏览器加载顺序
+  - 缓存
+  - 图片优化
+- 代码优化
+  - JS代码
+    - 尾递归
+  - CSS优化
+    - 3D加速
+
 
 ## Getting started
 
 ### Prerequisites
 
 1. Git
-1. Node: any 8.x version starting with 8.4.0 or greater
-1. Yarn: See [Yarn website for installation instructions](https://yarnpkg.com/lang/en/docs/install/)
-1. A fork of the repo (for any contributions)
-1. A clone of the [reactjs.org repo](https://github.com/reactjs/reactjs.org) on your local machine
+2. Node: any 8.x version starting with 8.4.0 or greater
+3. Yarn: See [Yarn website for installation instructions](https://yarnpkg.com/lang/en/docs/install/)
+
 
 ### Installation
 
-1. `cd reactjs.org` to go into the project root
-1. `yarn` to install the website's npm dependencies
+1. `cd blog` to go into the project root
+2. `yarn` to install the website's npm dependencies
 
 ### Running locally
 
 1. `yarn dev` to start the hot-reloading development server (powered by [Gatsby](https://www.gatsbyjs.org))
-1. `open http://localhost:8000` to open the site in your favorite browser
-
-## Contributing
-
-### Guidelines
-
-The documentation is divided into several sections with a different tone and purpose. If you plan to write more than a few sentences, you might find it helpful to get familiar with the [contributing guidelines](https://github.com/reactjs/reactjs.org/blob/master/CONTRIBUTING.md#guidelines-for-text) for the appropriate sections.
-
-### Create a branch
-
-1. `git checkout master` from any folder in your local `reactjs.org` repository
-1. `git pull origin master` to ensure you have the latest main code
-1. `git checkout -b the-name-of-my-branch` (replacing `the-name-of-my-branch` with a suitable name) to create a branch
-
-### Make the change
-
-1. Follow the "Running locally" instructions
-1. Save the files and check in the browser
-  1. Changes to React components in `src` will hot-reload
-  1. Changes to markdown files in `content` will hot-reload
-  1. If working with plugins, you may need to remove the `.cache` directory and restart the server
-
-### Test the change
-
-1. If possible, test any visual changes in all latest versions of common browsers, on both desktop and mobile.
-1. Run `yarn check-all` from the project root. (This will run Prettier, ESLint, and Flow.)
-
-### Push it
-
-1. `git add -A && git commit -m "My message"` (replacing `My message` with a commit message, such as `Fixed header logo on Android`) to stage and commit your changes
-1. `git push my-fork-name the-name-of-my-branch`
-1. Go to the [reactjs.org repo](https://github.com/reactjs/reactjs.org) and you should see recently pushed branches.
-1. Follow GitHub's instructions.
-1. If possible, include screenshots of visual changes. A Netlify build will also be automatically created once you make your PR so other people can see your change.
-
-## Translation
-
-If you are interesting in translating `reactjs.org`, please join the Crowdin.
-
-* [Crowdin - React](https://crowdin.com/project/react)
+2. `open http://localhost:8000` to open the site in your favorite browser
 
 ## Troubleshooting
 
 - `yarn reset` to clear the local cache
+
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Container from 'components/Container';
 import love from '../images/love.png';
-import book1 from '../images/1.jpeg'
-import book2 from '../images/2.jpeg'
-import book3 from '../images/3.jpeg'
-import book4 from '../images/4.jpeg'
-import book5 from '../images/5.jpeg'
-import book6 from '../images/6.jpeg'
+import book1 from '../images/1.jpeg';
+import book2 from '../images/2.jpeg';
+import book3 from '../images/3.jpeg';
+import book4 from '../images/4.jpeg';
+import book5 from '../images/5.jpeg';
+import book6 from '../images/6.jpeg';
+import { colors, media, fonts } from 'theme';
 
 class Resume extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = { active: 0 };
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick(idx) {
-    this.setState({ active: idx })
+    this.setState({ active: idx });
   }
   render() {
-    const { active } = this.state
+    const { active } = this.state;
     return (
       <Container css={{
         position: 'relative',
@@ -28,11 +29,20 @@ class Resume extends React.Component {
         }}>
           <h2 className="resume-title">About</h2>
           <p className="resume-p">Mello is a holy buddhist, a husband. He lives in HangZhou, ZheJiang with his wife Tyran.</p>
-          <img src={love} className="resume-love" />
+          <img src={love} className="resume-love" css={{
+            [media.greaterThan('small')]: {
+              width: '39.65%',
+              height: '59.53%',
+            },
+            [media.lessThan('small')]: {
+              width: '100%',
+              height: '150%',
+            },
+          }} />
           <p className="resume-p">
             Mello is currently a Frontend developer engineer and manager of the Frontend Platform team at
-          <a href="http://netease.com" target="_blank"> NetEase</a>. He originally joined the Listings team at NetEase in Apr 2017, tasked to revamp the event details pages from an archaic MySpace-style design to a modern, fully responsive layout.
-        </p>
+            <a href="http://netease.com" target="_blank"> NetEase</a>. He originally joined the Listings team at NetEase in Apr 2017, tasked to revamp the event details pages from an archaic MySpace-style design to a modern, fully responsive layout.
+          </p>
         </div>
         <Posts handleClick={this.handleClick} active={active} />
         {active === 0 && <WorkInfo />}
@@ -41,7 +51,7 @@ class Resume extends React.Component {
       </Container>
     );
   }
-};
+}
 
 const Posts = ({ active, handleClick }) => (
   <div css={{
@@ -51,7 +61,7 @@ const Posts = ({ active, handleClick }) => (
     letterSpacing: '1px',
     textAlign: 'center',
     fontSize: '12px',
-    fontWeight: 600
+    fontWeight: 600,
   }}>
     <a css={{
       cursor: 'pointer',
@@ -59,18 +69,18 @@ const Posts = ({ active, handleClick }) => (
       marginRight: 60,
       borderTop: active == 0 ? '1px solid #262626' : 'none',
       marginTop: active == 0 ? -1 : 0,
-      color: active === 0 ? '#262626' : '#999'
+      color: active === 0 ? '#262626' : '#999',
     }}
       onClick={handleClick.bind(null, 0)}>
       BIO
-          </a>
+    </a>
     <a css={{
       cursor: 'pointer',
       lineHeight: '52px',
       marginRight: 60,
       borderTop: active == 1 ? '1px solid #262626' : 'none',
       marginTop: active == 1 ? -1 : 0,
-      color: active === 1 ? '#262626' : '#999'
+      color: active === 1 ? '#262626' : '#999',
     }}
       onClick={handleClick.bind(null, 1)}
     >
@@ -81,13 +91,13 @@ const Posts = ({ active, handleClick }) => (
       marginRight: 60,
       borderTop: active == 2 ? '1px solid #262626' : 'none',
       marginTop: active == 2 ? -1 : 0,
-      color: active === 2 ? '#262626' : '#999'
+      color: active === 2 ? '#262626' : '#999',
     }}
       onClick={handleClick.bind(null, 2)}
     >
       BOOKS</a>
   </div>
-)
+);
 
 const WorkInfo = () => (
   <div css={{
@@ -105,7 +115,7 @@ const WorkInfo = () => (
       </div>
     </div>
   </div>
-)
+);
 const Projects = () => (
   <div css={{
     margin: '1em auto 1em',
@@ -114,31 +124,31 @@ const Projects = () => (
     <span className="resume-sub-title">react-magic</span>
     <p className="resume-p">
       A collection of magic animations for react components. Although React provides a way to implement arbitrary animations, it is not an easy task to do it, even for simple animations. That's where react-magic package comes in. It supports inline styles work with
-          <a href="https://github.com/Khan/aphrodite" target="_blank"> aphrodite</a>. Most animations was implemented base on
-          <a href="https://github.com/miniMAC/magic" target="_blank"> magic</a>.
-        </p>
+      <a href="https://github.com/Khan/aphrodite" target="_blank"> aphrodite</a>. Most animations was implemented base on
+      <a href="https://github.com/miniMAC/magic" target="_blank"> magic</a>.
+    </p>
     <span className="resume-sub-title">leaflet.migrationLayer</span>
     <p className="resume-p">
       leafet.migrationLayer is used to show migration data such as population,flight,vehicle,traffic and so on.Data visualization on map.
-        </p>
+    </p>
   </div>
-)
+);
 
-const BOOKS = [book1, book2, book3, book4, book5, book6]
+const BOOKS = [book1, book2, book3, book4, book5, book6];
 
 const Books = () => (
   <div css={{
     display: 'flex',
-    flexWrap:'wrap'
+    flexWrap: 'wrap',
   }}>
     {BOOKS.map((book, idx) => <img
       key={idx}
       src={book} css={{
         height: 200,
         width: 150,
-        margin:10
+        margin: 10,
       }} />)}
   </div>
-)
+);
 
 export default Resume;
