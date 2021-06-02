c0b95e3dde9686cbfeeb4395882779fc5d3370ee
hzzhaoxiangtao@corp.netease.com <sylvenas@outlook.com>
2021-06-02 21:11:32 +0800

    feat: img

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -4,64 +4,66 @@
  * @emails react-core
  */
 
-import React, {Component} from 'react';
-import {colors, media, fonts} from 'theme';
+import React, { Component } from "react";
+import { colors, media, fonts } from "theme";
 
 class DocSearch extends Component {
   state = {
-    enabled: true,
+    enabled: true
   };
   render() {
-    const {enabled} = this.state;
+    const { enabled } = this.state;
 
     return enabled ? (
       <div
         css={{
-          display: 'flex',
-          flex: '0 0 auto',
-          flexDirection: 'row',
-          alignItems: 'center',
-          paddingLeft: '0.5rem',
-          paddingRight: '0.5rem',
-          fontSize:15,
-          cursor: 'pointer',
-          [media.lessThan('small')]: {
-            justifyContent: 'flex-end',
+          display: "flex",
+          flex: "0 0 auto",
+          flexDirection: "row",
+          alignItems: "center",
+          paddingLeft: "0.5rem",
+          paddingRight: "0.5rem",
+          fontSize: 15,
+          cursor: "pointer",
+          [media.lessThan("small")]: {
+            justifyContent: "flex-end"
           },
-          [media.lessThan('large')]: {
-            marginRight: 10,
+          [media.lessThan("large")]: {
+            marginRight: 10
           },
-          [media.between('small', 'medium')]: {
-            width: 'calc(100% / 3)',
+          [media.between("small", "medium")]: {
+            width: "calc(100% / 3)"
           },
-          [media.between('medium', 'xlarge')]: {
-            width: 'calc(100% / 6)',
+          [media.between("medium", "xlarge")]: {
+            width: "calc(100% / 6)"
           },
-          [media.greaterThan('small')]: {
-            minWidth: 20,
-          },
-        }}>
+          [media.greaterThan("small")]: {
+            minWidth: 20
+          }
+        }}
+      >
         <a
           css={{
-            padding: '5px 10px',
+            padding: "5px 10px",
             marginLeft: 10,
-            whiteSpace: 'nowrap',
+            whiteSpace: "nowrap",
             ...fonts.small,
 
-            ':hover': {
-              color: colors.brand,
+            ":hover": {
+              color: colors.brand
             },
 
-            ':focus': {
+            ":focus": {
               outline: 0,
               backgroundColor: colors.lighter,
-              borderRadius: 15,
-            },
+              borderRadius: 15
+            }
           }}
-          href="https://github.com/lit-forest"
+          href="https://github.com/Sylvenas"
           target="_blank"
-          rel="noopener">
-          <i className="icon-github"></i>
+          rel="noopener"
+        >
+          <i className="icon-github" />
         </a>
       </div>
     ) : null;
